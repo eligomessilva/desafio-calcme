cb94eeed3e709ec9306d61d0fb9e2db3848345e9
Eli Gomes <silvagomes_s@hotmail.com>
2022-01-26 18:46:19 -0300

    Second commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'netflix'`, () => {
+  it(`should have as title 'desafio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('netflix');
+    expect(app.title).toEqual('desafio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('netflix app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('desafio app is running!');
   });
 });
